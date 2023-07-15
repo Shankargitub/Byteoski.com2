93399d8a44a006526e0e76309765612c5a559cdf
Shankargitub <shiv@byteoski.com>
2023-07-15 18:31:45 +0530

    Farmer and Consumer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react'; 
-// import {Page2} from './Provothon/page2'; 
+import {Page2} from './Provothon/page2'; 
 // import {Navbar} from './Byteoski3/Navbar';  
 // import {Header} from './Provothon/Header';  
-import {Dashabord} from './Extraction/Dashabord'; 
+// import {Dashabord} from './Extraction/Dashabord'; 
 
 const App = () => {
   return (
     <div>
       {/* <Page /> */}
       {/* <Navbar /> */}
-      {/* <Page2 />  */}
+      <Page2 /> 
       {/* <Header /> */}    
-        <Dashabord />  
+        {/* <Dashabord />   */}
 
     </div>
   );
